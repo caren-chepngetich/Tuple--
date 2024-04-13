2fa778a5c6a311a4f5220dceefc6197f032fa13e
Student <Caren-Chepngetich>
2024-04-13 17:34:04 +0300

    made changes

---
+++ index.py
@@ -0,0 +1,35 @@
+numbers = [8,9,10,11,12]
+print (tuple(numbers))
+
+# length
+
+print(len(numbers))
+
+# Slicing
+
+print(numbers[1])
+print(numbers[-2:])
+
+# Accessing values
+
+print(numbers[1])
+print(numbers[-1])
+
+# Repetition
+
+numbers = (numbers,)*2
+print(numbers)
+
+# Concatinate
+
+number = [1,2,34,5]
+words = ["Caren","Christine","Milly"]
+
+numberwords = number + words
+print(numberwords)
+
+#  Deleting
+
+del numbers
+print(numbers)
+
